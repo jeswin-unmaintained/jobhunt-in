ab2d1fb9795bdb2e64a54050598aa0be8c8390b1
Jeswin Kumar <jeswinpk@agilehead.com>
2016-04-23 23:24:57 +0530

    Get the bot to respond to requests. Send and receive messages.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")"
+./build.sh
 rsync -avz --delete --exclude '.git' -e ssh . chad@jobhunt.in:~/sites/jobhunt.in/
--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 import __babelPolyfill from "babel-polyfill";
 import koa from "koa";
 import routes from "koa-route";
+import bodyParser from 'koa-bodyparser';
 
 import * as yak from "./web/yak";
 import * as facebookAPI from "./api/facebook";
 
 const app = new koa();
+app.use(bodyParser());
 
 app.use(routes.get("/yak", yak.index));
-app.use(routes.get("/webhook", facebookAPI.webhook));
+app.use(routes.get("/api/fb/webhook", facebookAPI.webhook_get));
+app.use(routes.post("/api/fb/webhook", facebookAPI.webhook_post));
 
 app.listen(9001);
